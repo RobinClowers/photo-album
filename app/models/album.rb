5090f29f1c34a93ee220ee3ea901a43349ccfbd1
Robin Clowers <robin.clowers@gmail.com>
2014-03-07 20:52:31 -0800

    Add slug generation

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,5 +2,11 @@ class Album < ActiveRecord::Base
   has_many :photos, inverse_of: :album
   belongs_to :cover_photo, class_name: 'Photo'
 
+  before_create :generate_slug
+
   scope :active, -> { where.not(cover_photo_id: nil) }
+
+  def generate_slug
+    self.slug = title.parameterize
+  end
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Album do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".create" do
+    it "generates a slug before saving" do
+      Album.create!(title: 'Test Album')
+      Album.last.slug.should eq 'test-album'
+    end
+
+    it "with a duplicate slug raises" do
+      Album.create!(title: 'Test Album')
+      expect {
+        Album.create!(title: 'test album')
+      }.to raise_error ActiveRecord::RecordNotUnique
+    end
+  end
 end
