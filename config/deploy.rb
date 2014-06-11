6e07f8edd99db8cf35d9685a17696793caf4427c
Robin Clowers <robin.clowers@gmail.com>
2014-06-10 21:03:49 -0700

    Add puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,27 @@
-require 'capistrano/puma'
-
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
 set :application, 'photo-album'
 set :repo_url, 'git@github.com:RobinClowers/photo-album.git'
 set :puma_role, :app
+set :puma_env, -> { fetch(:rack_env, fetch(:rails_env, fetch(:stage))) }
+# Configure "min" to be the minimum number of threads to use to answer
+# requests and "max" the maximum.
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, -> { File.join(shared_path, 'tmp', 'pids', 'puma.state') }
+set :puma_pid, -> { File.join(shared_path, 'tmp', 'pids', 'puma.pid') }
+set :puma_bind, -> { File.join('unix://', shared_path, 'tmp', 'sockets', 'puma.sock') }
+set :puma_conf, -> { File.join(shared_path, 'puma.rb') }
+set :puma_access_log, -> { File.join(shared_path, 'log', 'puma_error.log') }
+set :puma_error_log, -> { File.join(shared_path, 'log', 'puma_access.log') }
+set :puma_init_active_record, false
+set :puma_preload_app, true
+
+set :default_env, {
+  'PATH' => '/opt/rubies/ruby-2.1.1/bin:$PATH',
+}
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,2 +1,3 @@
-server 'vagrant@10.0.0.2', roles: [:app]
+server 'deploy@10.0.0.2', roles: [:app]
 set :ssh_options, {keys: ['~/.vagrant.d/insecure_private_key']}
+set :ssh_options, {forward_agent: true}
