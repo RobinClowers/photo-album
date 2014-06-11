0397a564ba063c4cb7872463087e3b4f36992065
Robin Clowers <robin.clowers@gmail.com>
2014-06-10 22:05:36 -0700

    Set capstrano to deploy the current branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :default_env, {
 }
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/srv/photo_album'
