1a1c52dd6ea2fe636869b957adc49427c33e1ae5
Robin Clowers <robin.clowers@gmail.com>
2014-06-15 16:29:55 -0700

    Remove extraneous puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,20 +4,6 @@ lock '3.2.1'
 set :application, 'photo-album'
 set :repo_url, 'git@github.com:RobinClowers/photo-album.git'
 set :puma_role, :app
-set :puma_env, -> { fetch(:rack_env, fetch(:rails_env, fetch(:stage))) }
-# Configure "min" to be the minimum number of threads to use to answer
-# requests and "max" the maximum.
-set :puma_threads, [0, 16]
-set :puma_workers, 0
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, -> { File.join(shared_path, 'tmp', 'pids', 'puma.state') }
-set :puma_pid, -> { File.join(shared_path, 'tmp', 'pids', 'puma.pid') }
-set :puma_bind, -> { File.join('unix://', shared_path, 'tmp', 'sockets', 'puma.sock') }
-set :puma_conf, -> { File.join(shared_path, 'puma.rb') }
-set :puma_access_log, -> { File.join(shared_path, 'log', 'puma_error.log') }
-set :puma_error_log, -> { File.join(shared_path, 'log', 'puma_access.log') }
-set :puma_init_active_record, false
-set :puma_preload_app, true
 
 set :default_env, {
   'PATH' => '/opt/rubies/ruby-2.1.1/bin:$PATH',
