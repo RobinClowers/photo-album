ceddc9fc333cc5c4eab6e6f473551b60a13adc54
Robin Clowers <robin.clowers@gmail.com>
2014-07-17 21:47:18 -0700

    Set assets_roles

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ lock '3.2.1'
 set :application, 'photo-album'
 set :repo_url, 'git@github.com:RobinClowers/photo-album.git'
 set :puma_role, :app
+set :assets_roles, [:app]
 
 set :default_env, {
   'PATH' => '/opt/rubies/ruby-2.1.2/bin:$PATH',
