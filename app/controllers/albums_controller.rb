90119933be6efd6729b38aeb1e6cb341a77822c8
Robin Clowers <robin.clowers@gmail.com>
2014-10-18 18:16:48 -0500

    Handle redirects in albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,9 +1,18 @@
 class AlbumsController < ApplicationController
   expose(:albums) { Album.active }
-  expose(:album) { Album.find_by_slug!(params[:id]) }
+  expose(:album) { Album.find_by_slug!(slug) }
+  expose(:redirect) { Redirect.find_by_from(slug) }
   expose(:images) { album.photos.to_a }
 
   def index; end
 
-  def show; end
+  def show
+    redirect_to redirect.to if redirect
+  end
+
+  private
+
+  def slug
+    params[:id]
+  end
 end
