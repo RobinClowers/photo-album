54c53e9d3a987fde0cf128630ff70a0a9b412037
Robin Clowers <robin.clowers@gmail.com>
2014-10-18 11:52:58 -0500

    Set sidekiq_role to utility, add utility role

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,4 +1,4 @@
 server 'deploy@10.0.0.2', roles: [:web, :app, :db]
-server 'deploy@10.0.0.3', roles: [:app]
+server 'deploy@10.0.0.3', roles: [:app, :utility]
 set :ssh_options, {keys: ['~/.vagrant.d/insecure_private_key']}
 set :ssh_options, {forward_agent: true}
