95c20e41fe8897258bf16091cf26249c9ad6d15e
Robin Clowers <robin.clowers@gmail.com>
2014-12-03 21:21:13 -0400

    Hook up delete +1

--- app/controllers/photos/plus_ones_controller.rb
+++ app/controllers/photos/plus_ones_controller.rb
@@ -2,9 +2,11 @@ class Photos::PlusOnesController < ApplicationController
   before_filter :require_signed_in
   layout false
 
-  expose(:plus_one) { PlusOne.new(photo_id: photo_id, user: current_user) }
+  expose(:plus_one) { existing_plus_one || PlusOne.new(photo_id: photo_id, user: current_user) }
   expose(:count) { plus_one_count == 0 ? 1 : plus_one_count}
-  expose(:button_css_class) { existing_plus_one? ? 'voted' : '' }
+  expose(:button_css_class) { existing_plus_one ? 'voted' : '' }
+  expose(:plus_one_form_url) { existing_plus_one ? destroy_path : photo_plus_ones_path }
+  expose(:plus_one_form_method) { existing_plus_one ? :delete : :post }
 
   def index
   end
@@ -13,6 +15,13 @@ class Photos::PlusOnesController < ApplicationController
     create_plus_one
     render 'index', status: :created
   end
+
+  def destroy
+    existing_plus_one.destroy
+    render 'index', status: :ok
+  end
+
+  private
 
   def create_plus_one
     !plus_one.save
@@ -27,7 +36,11 @@ class Photos::PlusOnesController < ApplicationController
     @plus_one_count ||= PlusOne.where(photo_id: photo_id).count
   end
 
-  def existing_plus_one?
-    PlusOne.where(photo_id: photo_id, user: current_user).exists?
+  def existing_plus_one
+    PlusOne.where(photo_id: photo_id, user: current_user).first
+  end
+
+  def destroy_path
+    photo_plus_one_path(existing_plus_one, photo_id: photo_id)
   end
 end
