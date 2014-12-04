94155265fc4413218de12d9808641f0fb6c8244e
Robin Clowers <robin.clowers@gmail.com>
2014-12-04 18:36:55 -0400

    Extract plus one form object

--- app/controllers/photos/plus_ones_controller.rb
+++ app/controllers/photos/plus_ones_controller.rb
@@ -2,42 +2,18 @@ class Photos::PlusOnesController < ApplicationController
   before_filter :require_signed_in
   layout false
 
-  expose(:plus_one) { existing_plus_one || PlusOne.new(photo_id: photo_id, user: current_user) }
-  expose(:plus_one_count) { PlusOne.where(photo_id: photo_id).count }
-  expose(:button_text) { plus_one_count == 0 ? 1 : plus_one_count}
-  expose(:button_css_class) { existing_plus_one ? 'square-button active' : 'square-button' }
-  expose(:plus_one_form_url) { existing_plus_one ? destroy_path : photo_plus_ones_path }
-  expose(:plus_one_form_method) { existing_plus_one ? :delete : :post }
+  expose(:plus_one) { PlusOneForm.new(params.merge(current_user: current_user, router: self)) }
 
   def index
   end
 
   def create
-    create_plus_one
+    plus_one.save
     render 'index', status: :created
   end
 
   def destroy
-    existing_plus_one.destroy
+    plus_one.destroy
     render 'index', status: :ok
-  end
-
-  private
-
-  def create_plus_one
-    !plus_one.save
-  rescue ActiveRecord::RecordNotUnique
-  end
-
-  def photo_id
-    params[:photo_id]
-  end
-
-  def existing_plus_one
-    PlusOne.where(photo_id: photo_id, user: current_user).first
-  end
-
-  def destroy_path
-    photo_plus_one_path(existing_plus_one, photo_id: photo_id)
   end
 end
---
+++ app/models/plus_one_form.rb
@@ -0,0 +1,62 @@
+class PlusOneForm
+  include ActiveModel::Model
+
+  def initialize(params)
+    @photo_id = params[:photo_id]
+    @current_user = params[:current_user]
+    @router = params[:router]
+  end
+
+  def self.model_name
+    PlusOne.model_name
+  end
+
+  def save
+    model.save
+  rescue ActiveRecord::RecordNotUnique
+  end
+
+  def destroy
+    existing_plus_one.destroy if existing_plus_one
+  end
+
+  def count
+    PlusOne.where(photo_id: photo_id).count
+  end
+
+  def button_text
+    count == 0 ? 1 : count
+  end
+
+  def button_css_class
+    existing_plus_one ? 'square-button active' : 'square-button'
+  end
+
+  def form_url
+    existing_plus_one ? destroy_path : create_path
+  end
+
+  def form_method
+    existing_plus_one ? :delete : :post
+  end
+
+  private
+
+  attr_accessor :photo_id, :current_user, :router
+
+  def model
+    existing_plus_one || PlusOne.new(photo_id: photo_id, user: current_user)
+  end
+
+  def existing_plus_one
+    PlusOne.where(photo_id: photo_id, user: current_user).first
+  end
+
+  def destroy_path
+    router.photo_plus_one_path(existing_plus_one, photo_id: photo_id)
+  end
+
+  def create_path
+    router.photo_plus_ones_path
+  end
+end
