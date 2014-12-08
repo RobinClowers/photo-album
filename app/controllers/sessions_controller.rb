b951a883f282508b0e2df7ec9bf05121e451e3aa
Robin Clowers <robin.clowers@gmail.com>
2014-12-08 14:13:03 -0400

    Return users to the correct page on sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,15 @@ class SessionsController < ApplicationController
 
   def create
     session[:user_id] = user.id
-    redirect_to session[:return_url] || root_path
+    redirect_to return_path
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out"
+    redirect_to return_path
   end
+
+  private
 
   def auth
     request.env["omniauth.auth"]
@@ -21,5 +23,9 @@ class SessionsController < ApplicationController
 
   def existing_user
     @user ||= User.where(provider: auth["provider"], uid: auth["uid"]).first
+  end
+
+  def return_path
+    session[:return_url] || root_path
   end
 end
