14028620cae0c7108052e3d69e8acde7acffbbc1
Robin Clowers <robin.clowers@gmail.com>
2014-12-09 16:20:33 -0400

    Add production smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ PhotoAlbum::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com',
+    port:                 587,
+    domain:               'photos.robinclowers.com',
+    user_name:            ENV['SMTP_USER_NAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
