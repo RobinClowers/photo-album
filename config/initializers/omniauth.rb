70ccbf995478f9bb9035994c7c561a23af357f18
Robin Clowers <robin.clowers@gmail.com>
2014-12-09 16:44:32 -0400

    Use fb login 2.0

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+   :client_options => {
+      :site => 'https://graph.facebook.com/v2.0',
+      :authorize_url => "https://www.facebook.com/v2.0/dialog/oauth"
+    }
 end
