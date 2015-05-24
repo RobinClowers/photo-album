27dcca143cff1341c88acf32c53d9ec04367e655
Robin Clowers <robin.clowers@gmail.com>
2015-05-23 20:53:34 -0600

    Add offline mode
    Allows photos to be loaded from the dev server instead of s3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,18 +4,51 @@ class Photo < ActiveRecord::Base
   default_scope -> { order(:filename) }
 
   def thumb_url
-    File.join(Rails.application.config.base_photo_url, path, 'thumbs', filename)
+    File.join(protocol,base_path, path, 'thumbs', filename)
   end
 
   def url
-    File.join(Rails.application.config.base_photo_url, path, filename)
+    insecure_url
   end
 
   def insecure_url
-    File.join("http://", Rails.application.config.base_photo_url, path, filename)
+    File.join(protocol, base_path, path, filename)
   end
 
   def secure_url
-    File.join(Rails.application.config.base_secure_photo_url, path, filename)
+    File.join(protocol(secure: true), base_path(secure: true), path, filename)
+  end
+
+  def protocol(secure: false)
+    if offline?
+      ''
+    elsif secure
+      'https://'
+    else
+      'http://'
+    end
+  end
+
+  def path
+    if offline?
+      super.titleize
+    else
+      super
+    end
+  end
+
+  def base_path(secure: false)
+    if offline?
+      ENV['OFFLINE_PATH']
+    elsif secure
+      Rails.application.config.base_secure_photo_url
+    else
+      Rails.application.config.base_photo_url
+    end
+  end
+
+  private
+  def offline?
+    ENV['OFFLINE'] == 'true'
   end
 end
