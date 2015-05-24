417a995d7e013818c433b15319d26f41326ee342
Robin Clowers <robin.clowers@gmail.com>
2015-05-23 21:07:10 -0600

    Update jquery tag to use offline flag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def jquery_url
+    if ENV['OFFLINE'] == 'true'
+       javascript_include_tag "jquery"
+    else
+      tag('script', src: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js')
+    end
+  end
 end
