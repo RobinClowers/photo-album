8ac7463ab3708075cef71a9d2c739ad3579ba288
Robin Clowers <robin.clowers@gmail.com>
2015-05-30 13:46:37 -0500

    Fix jquery script tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,7 @@ module ApplicationHelper
     if ENV['OFFLINE'] == 'true'
        javascript_include_tag "jquery"
     else
-      tag('script', src: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js')
+      javascript_include_tag 'http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js'
     end
   end
 end
