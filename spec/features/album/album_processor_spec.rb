923003563f39c19936332aaf1b36f3f56af0e215
Robin Clowers <robin.clowers@gmail.com>
2015-10-04 17:24:14 -0700

    Add basic album processor specs

---
+++ spec/features/album/album_processor_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe AlbumProcessor do
+  let(:base_path) { Rails.root.join("spec", "fixtures", "files", "photos") }
+  let(:web_path) { base_path.join("web") }
+  let(:thumb_path) { base_path.join("thumbs") }
+  let!(:originals) { Dir.entries(base_path).select { |f| f =~ /\.jpg|png\Z/i } }
+  let(:web_version_path) { web_path.join(originals.first) }
+  let(:thumb_version_path) { thumb_path.join(originals.first) }
+  let(:image) { double(:image).as_null_object }
+  subject(:processor) { AlbumProcessor.new(base_path) }
+
+  describe "#process_all" do
+    def mock_image
+      allow(Magick::ImageList).to receive(:new) { image }
+      allow(image).to receive(:resize_to_fit) { image }
+      allow(image).to receive(:resize_to_fill) { image }
+    end
+
+    it "create the versions on disk" do
+      processor.process_all
+      expect(File.exists?(web_version_path)).to be true
+      expect(File.exists?(thumb_version_path)).to be true
+    end
+
+    it "skips processed photos" do
+      allow(File).to receive(:exists?) { true }
+      processor.process_all
+      expect(image).not_to have_received(:resize_to_fit)
+      expect(image).not_to have_received(:resize_to_fill)
+    end
+
+    it "resizes images for versions" do
+      mock_image
+      processor.process_all
+      expect(image).to have_received(:resize_to_fit).with(1024, 1024).once
+      expect(image).to have_received(:resize_to_fill).with(75, 75).once
+    end
+
+    after do
+      FileUtils.remove_dir web_path
+      FileUtils.remove_dir thumb_path
+    end
+  end
+end
