13cce1e7787afa31ec9a6c1da27ef6d282621bf5
Robin Clowers <robin.clowers@gmail.com>
2015-10-04 18:29:49 -0700

    Add a new image size called small

--- spec/features/album/album_processor_spec.rb
+++ spec/features/album/album_processor_spec.rb
@@ -3,9 +3,11 @@ require "spec_helper"
 describe AlbumProcessor do
   let(:base_path) { Rails.root.join("spec", "fixtures", "files", "photos") }
   let(:web_path) { base_path.join("web") }
+  let(:small_path) { base_path.join("small") }
   let(:thumb_path) { base_path.join("thumbs") }
   let!(:originals) { Dir.entries(base_path).select { |f| f =~ /\.jpg|png\Z/i } }
   let(:web_version_path) { web_path.join(originals.first) }
+  let(:small_version_path) { small_path.join(originals.first) }
   let(:thumb_version_path) { thumb_path.join(originals.first) }
   let(:image) { double(:image).as_null_object }
   subject(:processor) { AlbumProcessor.new(base_path) }
@@ -20,6 +22,7 @@ describe AlbumProcessor do
     it "create the versions on disk" do
       processor.process_all
       expect(File.exists?(web_version_path)).to be true
+      expect(File.exists?(small_version_path)).to be true
       expect(File.exists?(thumb_version_path)).to be true
     end
 
@@ -34,11 +37,13 @@ describe AlbumProcessor do
       mock_image
       processor.process_all
       expect(image).to have_received(:resize_to_fit).with(1024, 1024).once
+      expect(image).to have_received(:resize_to_fit).with(320, 320).once
       expect(image).to have_received(:resize_to_fill).with(75, 75).once
     end
 
     after do
       FileUtils.remove_dir web_path
+      FileUtils.remove_dir small_path
       FileUtils.remove_dir thumb_path
     end
   end
