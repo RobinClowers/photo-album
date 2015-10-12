0a69b831381e7410fc9cc3dbaf95d9f3fcb8b6d8
Robin Clowers <robin.clowers@gmail.com>
2015-10-11 18:27:18 -0700

    Only process photos in that album

--- app/controllers/admin/process_version_jobs_controller.rb
+++ app/controllers/admin/process_version_jobs_controller.rb
@@ -3,6 +3,7 @@ class Admin::ProcessVersionJobsController < Admin::ApplicationController
     if Photo::VALID_VERSIONS.include?(version)
       if album
         if photo_filenames
+          reject_request and return unless album_contains_all_photos
           ProcessPhotosWorker.perform_async(album, photo_filenames, [version])
         else
           process_album(album)
@@ -14,11 +15,20 @@ class Admin::ProcessVersionJobsController < Admin::ApplicationController
       end
       head :created
     else
-      render nothing: true, status: :unprocessable_entity
+      reject_request
     end
   end
 
   private
+
+  def reject_request
+    render nothing: true, status: :unprocessable_entity
+  end
+
+  def album_contains_all_photos
+    album_photos = Album.find_by_slug(album).photos.pluck(:filename)
+    photo_filenames.all? { |filename| album_photos.include?(filename) }
+  end
 
   def process_album(slug)
     ProcessAlbumWorker.perform_async(slug, [version])
