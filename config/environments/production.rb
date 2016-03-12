0143793eb4a13534e4b6d01b4b357f4002991efc
Robin Clowers <robin.clowers@gmail.com>
2016-03-12 11:37:46 -0800

    Switch to mailgun for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.mandrillapp.com',
+    address:              'smtp.mailgun.org',
     port:                 587,
     domain:               'photos.robinclowers.com',
     user_name:            ENV['SMTP_USER_NAME'],
