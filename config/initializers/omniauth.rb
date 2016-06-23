7e73c82a9381514d4ba0ec5de7c3e9ab61f5d066
Robin Clowers <robin.clowers@gmail.com>
2016-06-22 19:17:11 -0700

    Update to opengraph 2.6

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
    :client_options => {
-      :site => 'https://graph.facebook.com/v2.0',
-      :authorize_url => "https://www.facebook.com/v2.0/dialog/oauth"
+      :site => 'https://graph.facebook.com/v2.6',
+      :authorize_url => "https://www.facebook.com/v2.6/dialog/oauth"
     }
 end
