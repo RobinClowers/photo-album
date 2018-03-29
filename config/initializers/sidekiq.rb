2bbbdbfde4c21ba35ad5e75b8c51da8b184a3bc5
Robin Clowers <robin.clowers@gmail.com>
2018-03-29 11:40:32 +1100

    Configure redis password

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,12 @@
+redis_config = {
+  url: ENV['REDIS_URL'],
+  password: ENV['REDIS_PASSWORD'],
+}
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = redis_config
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = redis_config
 end
