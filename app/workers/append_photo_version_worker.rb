41a60c5e3d7447d0464efcaa71074a981d996299
Robin Clowers <robin.clowers@gmail.com>
2018-06-11 17:10:23 +0700

    Improve AppendPhotoVersionWorker error message

--- app/workers/append_photo_version_worker.rb
+++ app/workers/append_photo_version_worker.rb
@@ -4,6 +4,7 @@ class AppendPhotoVersionWorker
 
   def perform(album_slug, filename, version)
     album = Album.find_by_slug(album_slug)
+    raise "Album not found with slug: #{album_slug}" unless album
     album.photos.find_by_filename(filename).has_version!(version)
   end
 end
