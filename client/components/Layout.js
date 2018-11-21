6fd35fe0d3f11f7e54d748d488a7ef4e32fe0464
Robin Clowers <robin.clowers@gmail.com>
2018-11-20 22:49:53 -0700

    Set up jss properly
    This seems to fix the weird style issues I had ever since I installed
    Material UI.

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,36 +1,11 @@
-import Head from 'next/head'
-import { createMuiTheme } from '@material-ui/core/styles';
-import orange from '@material-ui/core/colors/orange';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from 'components/AppBar'
 
-const theme = createMuiTheme({
-  typography: {
-    useNextVariants: true
-  },
-  palette: {
-    primary: {
-      main: '#3949ab',
-    },
-    secondary: orange,
-  },
-})
 const Layout = ({user, children}) => (
-  <React.Fragment>
-    <Head>
-      <title>Robin's Photos</title>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-      />
-    </Head>
-    <CssBaseline />
-      <AppBar user={user} />
-      {children}
-    <CssBaseline />
-  </React.Fragment>
+  <CssBaseline>
+    <AppBar user={user} />
+    {children}
+  </CssBaseline>
 )
 
 export default Layout
