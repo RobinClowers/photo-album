9878ea983752f8c9712e112f763f8d0cd9e86cbf
Robin Clowers <robin.clowers@gmail.com>
2018-11-21 17:58:04 -0700

    Use webpack define to replace process.env
    This is so we can use env vars on the client.

--- client/next.config.js
+++ client/next.config.js
@@ -1,1 +1,13 @@
-module.exports = () => ({})
+const webpack = require('webpack')
+
+module.exports = () => ({
+  webpack: (config, {}) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.API_SCHEME': JSON.stringify(process.env.API_SCHEME),
+        'process.env.API_HOST': JSON.stringify(process.env.API_HOST),
+      })
+    )
+    return config
+  },
+})
