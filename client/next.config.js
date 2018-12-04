9cfae715fe56eeadb70920ad1a9380b1d77c8985
Robin Clowers <robin.clowers@gmail.com>
2018-12-04 16:10:26 -0600

    No webpack in production server
    This works around an issue with the now/next builder:
    https://github.com/zeit/now-builders/issues/61.

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,23 @@
-const webpack = require('webpack')
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === "development"
+    ? require("next/constants")
+    : require("next-server/constants");
 
-module.exports = () => ({
-  webpack: (config, {}) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.API_SCHEME': JSON.stringify(process.env.API_SCHEME),
-        'process.env.API_HOST': JSON.stringify(process.env.API_HOST),
-      })
-    )
-    return config
-  },
-})
+module.exports = (phase) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {}
+  }
+
+  const webpack = require('webpack')
+  return {
+    webpack: (config, {}) => {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env.API_SCHEME': JSON.stringify(process.env.API_SCHEME),
+          'process.env.API_HOST': JSON.stringify(process.env.API_HOST),
+        })
+      )
+      return config
+    },
+  }
+}
