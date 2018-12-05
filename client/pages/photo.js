523bddc375ddcd1cf9271d96ed8f1ebbf0254064
Robin Clowers <robin.clowers@gmail.com>
2018-12-04 19:31:28 -0600

    Extract FullScreenPhoto

--- client/pages/photo.js
+++ client/pages/photo.js
@@ -3,6 +3,7 @@ import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Layout from 'client/components/Layout'
 import ChangePhotoButton from 'client/components/ChangePhotoButton'
+import FullScreenPhoto from 'client/components/FullScreenPhoto'
 import { getPhoto } from 'client/src/api'
 import debounce from 'lodash/debounce'
 
@@ -19,86 +20,15 @@ const styles = theme => ({
   caption: {
     marginTop: 4,
   },
-  image: {
-    position: 'relative',
-    left: '50%',
-    transform: 'translateX(-50%)',
-  },
 })
 
-const ceiling = (num, ceiling) => (num > ceiling) ? ceiling : num
-
-const byRatio = (initial, ratio) => Math.round(initial * ratio)
-
-const maxWidth = () => global.innerWidth
-
-const maxHeight = () => global.innerHeight - headerHeight - captionHeight
-
 class Photo extends React.Component {
-  constructor(props) {
-    super(props)
-    this.image = React.createRef()
-    this.state = {
-      imageWidth: undefined,
-      imageHeight: undefined,
-    }
-  }
-
   static async getInitialProps({ query }) {
     if (!query.slug) return { error: true }
     if (!query.filename) return { error: true }
 
     const res = await getPhoto(query.slug, query.filename)
     return res
-  }
-
-  componentDidMount() {
-    window.addEventListener(
-      'resize',
-      debounce(() => this.forceUpdate(), 20, false),
-      false)
-
-    const element = this.image.current
-    if (element && element.complete) {
-        this.imageLoaded()
-    }
-  }
-
-  componentDidUpdate({ photo }) {
-    if (this.props.photo !== photo) {
-      this.setState({ imageWidth: undefined, imageHeight: undefined })
-    }
-  }
-
-  imageLoaded = _event => {
-    this.setState({
-      imageWidth: this.image.current.width,
-      imageHeight: this.image.current.height,
-    })
-  }
-
-  calculateImageDimensions() {
-    const { imageWidth, imageHeight } = this.state
-    if (!imageWidth) return undefined
-
-    if (this.heightRatio() > this.widthRatio()) {
-      return {
-        width: byRatio(imageWidth, this.widthRatio()),
-        height: byRatio(imageHeight, this.widthRatio()),
-      }
-    }
-    return {
-      width: byRatio(imageWidth, this.heightRatio()),
-      height: byRatio(imageHeight, this.heightRatio()),
-    }
-  }
-
-  heightRatio() {
-    return ceiling(maxHeight() / this.state.imageHeight, 1)
-  }
-
-  widthRatio() {
-    return ceiling(maxWidth() / this.state.imageWidth, 1)
   }
 
   render() {
@@ -126,15 +56,9 @@ class Photo extends React.Component {
                 albumSlug={album.slug}
                 photoFilename={previous_photo_filename} />
             }
-            <img
-              style={{
-                display: this.state.imageWidth ? 'block' : 'none',
-                ...this.calculateImageDimensions(),
-              }}
-              ref={this.image}
-              onLoad={this.imageLoaded}
-              src={photo.url}
-              className={classes.image} />
+            <FullScreenPhoto
+              photo_url={photo.url}
+              topOffset={headerHeight + captionHeight} />
             {next_photo_filename &&
               <ChangePhotoButton
                 variant="next"
