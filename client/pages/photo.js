a46bec775dcc14e75ea65b682536ef0b2912c17a
Robin Clowers <robin.clowers@gmail.com>
2018-12-04 18:53:43 -0600

    Style buttons

--- client/pages/photo.js
+++ client/pages/photo.js
@@ -1,8 +1,9 @@
 import Error from 'next/error'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
+import ButtonBase from '@material-ui/core/ButtonBase'
 import Icon from '@material-ui/core/Icon'
+import { fade } from '@material-ui/core/styles/colorManipulator';
 import Layout from 'client/components/Layout'
 import { getPhoto } from 'client/src/api'
 import { Link } from 'client/routes'
@@ -11,9 +12,34 @@ import debounce from 'lodash/debounce'
 const headerHeight = 64
 const captionHeight = 20
 
+const button = theme => ({
+  position: 'absolute',
+  transform: 'translateY(-50%)',
+  top: '50%',
+  height: '100%',
+  zIndex: 2,
+  opacity: 0,
+  transition: theme.transitions.create(
+    'background-color', {
+      duration: theme.transitions.duration.shortest,
+    }
+  ),
+  width: '30%',
+  '&:hover': {
+    opacity: 1,
+  },
+})
+
+const buttonIcon = {
+  position: 'relative',
+  fontSize: '2em',
+}
+
 const styles = theme => ({
   container: {
     textAlign: 'center',
+  },
+  photoContainer: {
     position: 'relative',
   },
   caption: {
@@ -24,15 +50,35 @@ const styles = theme => ({
     left: '50%',
     transform: 'translateX(-50%)',
   },
+  button: {
+    borderRadius: '50%',
+    color: theme.palette.common.white,
+    padding: theme.spacing.unit * 2,
+    transition: theme.transitions.create(
+      'background-color', {
+        duration: theme.transitions.duration.shortest,
+      }
+    ),
+    backgroundColor: fade(theme.palette.action.active, theme.palette.action.hoverOpacity),
+    // Reset on touch devices, it doesn't add specificity
+    '@media (hover: none)': {
+      backgroundColor: 'transparent',
+    },
+  },
   previousButton: {
-    position: 'absolute',
-    top: '50%',
+    ...button(theme),
     left: 0,
-    zIndex: 2,
+  },
+  buttonIconPrevious: {
+    ...buttonIcon,
+    left: '6px',
+  },
+  buttonIconNext: {
+    ...buttonIcon,
+    left: '2px',
   },
   nextButton: {
-    position: 'absolute',
-    top: '50%',
+    ...button(theme),
     right: 0,
   },
 })
@@ -130,36 +176,42 @@ class Photo extends React.Component {
     return (
       <Layout user={user} pageContext={this.props.pageContext}>
         <div className={classes.container}>
-          {previous_photo_filename &&
-            <Link
-              route='photo'
-              params={{slug: album.slug, filename: previous_photo_filename}}>
-              <IconButton className={classes.previousButton} aria-label="next">
-                <Icon>arrow_left</Icon>
-              </IconButton>
-            </Link>
-          }
-          <img
-            style={{
-              display: this.state.imageWidth ? 'block' : 'none',
-              ...this.calculateImageDimensions(),
-            }}
-            ref={this.image}
-            onLoad={this.imageLoaded}
-            src={photo.url}
-            className={classes.image} />
+          <div className={classes.photoContainer}>
+            {previous_photo_filename &&
+              <Link
+                route='photo'
+                params={{slug: album.slug, filename: previous_photo_filename}}>
+                <ButtonBase className={classes.previousButton} aria-label="next" disableRipple={true}>
+                  <div className={classes.button}>
+                    <Icon className={classes.buttonIconPrevious}>arrow_back_ios</Icon>
+                  </div>
+                </ButtonBase>
+              </Link>
+            }
+            <img
+              style={{
+                display: this.state.imageWidth ? 'block' : 'none',
+                ...this.calculateImageDimensions(),
+              }}
+              ref={this.image}
+              onLoad={this.imageLoaded}
+              src={photo.url}
+              className={classes.image} />
+            {next_photo_filename &&
+              <Link
+                route='photo'
+                params={{slug: album.slug, filename: next_photo_filename}}>
+                <ButtonBase className={classes.nextButton} aria-label="next" disableRipple={true}>
+                  <div className={classes.button}>
+                    <Icon className={classes.buttonIconNext}>arrow_forward_ios</Icon>
+                  </div>
+                </ButtonBase>
+              </Link>
+            }
+          </div>
           <Typography className={classes.caption} variant="caption" color="inherit">
             {photo.caption}
           </Typography>
-          {next_photo_filename &&
-            <Link
-              route='photo'
-              params={{slug: album.slug, filename: next_photo_filename}}>
-              <IconButton className={classes.nextButton} aria-label="next">
-                <Icon>arrow_right</Icon>
-              </IconButton>
-            </Link>
-          }
         </div>
       </Layout>
     )
