32692cd524a0473f9b60db942cd13db2c48bf582
Robin Clowers <robin.clowers@gmail.com>
2018-12-06 11:39:15 -0600

    Set cookie domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_photo-album_session'
+Rails.application.config.session_store :cookie_store,
+  key: '_photo-album_session',
+  domain: ENV.fetch("COOKIE_DOMAIN") { "localhost" }
