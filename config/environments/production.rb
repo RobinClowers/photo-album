0f371a1dd555bf6399ffd5c8b9ffe4076dd7ccbf
Robin Clowers <robin.clowers@gmail.com>
2018-12-09 11:25:36 -0600

    Bring back smtp settings
    Looks like I accidentally deleted this during a rails upgrade.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "photo_album_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mailgun.org',
+    port:                 587,
+    domain:               'photos.robinclowers.com',
+    user_name:            ENV['SMTP_USER_NAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
