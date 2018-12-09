b18d0c558135c50c785fc3fcb7da78551211272c
Robin Clowers <robin.clowers@gmail.com>
2018-12-09 12:08:48 -0600

    Only use exception_notification in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,10 @@ Rails.application.configure do
     end
   end
 
+  config.middleware.use ExceptionNotification::Rack,
+    :email => {
+      :email_prefix => "[PhotoAlbum Error] ",
+      :sender_address => %{"PhotoAlbum Error Notifier" <errors@photos.robinclowers.com>},
+      :exception_recipients => %w{robin.clowers@gmail.com}
+    }
 end
--- config/initializers/exception_notification.rb
+++
@@ -1,6 +0,0 @@
-PhotoAlbum::Application.config.middleware.use ExceptionNotification::Rack,
-  :email => {
-    :email_prefix => "[PhotoAlbum Error] ",
-    :sender_address => %{"PhotoAlbum Error Notifier" <errors@photos.robinclowers.com>},
-    :exception_recipients => %w{robin.clowers@gmail.com}
-  }
