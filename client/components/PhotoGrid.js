9bff179264e472f7947513d7c98a2d78c22a25d9
Robin Clowers <robin.clowers@gmail.com>
2018-12-30 21:08:27 -0600

    Convert PhotoGrid to react hooks

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import justify from 'justified-layout'
 import PhotoGridItem from 'client/components/PhotoGridItem'
 import { withStyles } from '@material-ui/core/styles'
@@ -13,52 +14,54 @@ const styles = theme => ({
   },
 })
 
-const buildGrid = (photos, width = global.innerWidth - gutter * 2) => {
+const gridWidth = windowWidth => {
+  if (windowWidth < 425) {
+    return windowWidth
+  }
+  return windowWidth - gutter * 2
+}
+
+const buildGrid = (photos, windowWidth) => {
   const dimensions = photos.map(p => {
     const { original } = p.versions
     return { height: original.height, width: original.width }
   })
-  const result = justify(dimensions, { containerWidth: width })
+  const result = justify(dimensions, { containerWidth: gridWidth(windowWidth) })
   return result.boxes.map((box, i) => ({ ...box, photo: photos[i] }))
 }
 
-class PhotoGrid extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isClient: false,
+const PhotoGrid = ({ photos, albumSlug, classes }) => {
+  const [isClient, updateIsClient] = useState(false)
+  useEffect(() => {
+    updateIsClient(true)
+    window.addEventListener('resize', handleResize, false)
+    return () => {
+      window.removeEventListener('resize', handleResize, false)
     }
-  }
+  }, [])
 
-  componentDidMount() {
-    this.setState({ ...this.state, isClient: true })
-    window.addEventListener('resize', this.handleResize, false)
-  }
+  const [windowWidth, updateWindowWidth] = useState()
+  useEffect(() => {
+    updateWindowWidth(window.innerWidth)
+  }, [])
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize, false)
-  }
+  const handleResize = debounce(() => updateWindowWidth(window.innerWidth), 20, false)
 
-  handleResize = debounce(() => this.forceUpdate(), 20, false)
-
-  render() {
-    const { photos, albumSlug, classes } = this.props
-
-    return (
-      <div className={classes.container}>
-        {this.state.isClient ?
-          buildGrid(photos).map(item => (
+  return (
+    <div className={classes.container}>
+      {isClient ?
+        buildGrid(photos, windowWidth).map(item => (
+          <PhotoGridItem item={item} albumSlug={albumSlug} key={item.photo.id} />
+        ))
+      :
+        <div style={{display: 'none'}}>
+          {buildGrid(photos, 1060).map(item => (
             <PhotoGridItem item={item} albumSlug={albumSlug} key={item.photo.id} />
-          ))
-        :
-          <div style={{display: 'none'}}>
-            {buildGrid(photos, 1060).map(item => (
-              <PhotoGridItem item={item} albumSlug={albumSlug} key={item.photo.id} />
-            ))}
-          </div>
-        }
-      </div>
-    )
-  }
+          ))}
+        </div>
+      }
+    </div>
+  )
 }
+
 export default withStyles(styles)(PhotoGrid)
