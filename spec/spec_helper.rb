cae27fb303d9367ba3e195d7696c8e8a4de4b23c
Robin Clowers <robin.clowers@gmail.com>
2019-01-01 13:17:10 -0600

    Configure vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,19 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'vcr'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/cassettes"
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   config.before(:each) do
