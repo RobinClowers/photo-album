05204868f45fc243ae5619c4cf880ea9a32d2d55
Robin Clowers <robin.clowers@gmail.com>
2019-01-04 11:02:59 -0600

    Remove unused CoverPhotoValidator

--- app/features/photo/cover_photo_validator.rb
+++
@@ -1,6 +0,0 @@
-class CoverPhotoValidator < ActiveModel::EachValidator
-  def validate_each(album, attribute, value)
-    return unless album.photos.include?(album[:attribute])
-    album.errors[:attribute].add("cover photo isn't in this album")
-  end
-end
