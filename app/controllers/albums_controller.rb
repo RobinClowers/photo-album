57ceb4d6ad4a568df0b8db3831033022dbf0b867
Robin Clowers <robin.clowers@gmail.com>
2019-01-07 23:35:28 -0600

    Use a query for album show

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,8 +2,9 @@ class AlbumsController < ApplicationController
   respond_to :json
 
   expose(:album_list) { AlbumListQuery.new(current_user: current_user) }
-  expose(:album) { album_list.active.includes(
-    { photos: :versions }, { cover_photo: :versions }).find_by_slug!(slug) }
+  expose(:album) {
+    AlbumQuery.new(slug: slug, album_list: album_list, current_user: current_user)
+  }
   expose(:redirect) { Redirect.find_by_from(slug) }
   expose(:images) { album.photos.to_a }
 
@@ -13,10 +14,7 @@ class AlbumsController < ApplicationController
 
   def show
     redirect_to redirect.to if redirect
-    render json: {
-      user: current_user,
-      album: album.as_json(include: [:photos, :cover_photo]),
-    }
+    render json: album
   end
 
   private
---
+++ app/queries/album_query.rb
@@ -0,0 +1,24 @@
+class AlbumQuery
+  attr_reader :slug, :album_list, :album, :current_user
+
+  def initialize(params)
+    @current_user = params[:current_user]
+    @slug = params[:slug]
+    @album_list ||= AlbumListQuery.new(current_user: current_user)
+    @album = album_list.active.includes(
+      { photos: :versions }, { cover_photo: :versions }).find_by_slug!(slug)
+  end
+
+  def as_json(_options = {})
+    cover_photo = album.cover_photo
+    {
+      user: current_user,
+      album: album.as_json(include: [:photos, :cover_photo]),
+      share_photo: {
+        url: cover_photo.original_version.url,
+        width: cover_photo.original_version.width,
+        height: cover_photo.original_version.height,
+      },
+    }
+  end
+end
