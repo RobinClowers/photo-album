eb59860c4101312651005be156e1ef209b12c478
Robin Clowers <robin.clowers@gmail.com>
2021-04-25 16:26:54 -0700

    Remove omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    stored_location_for(resource) || root_path
   end
 
   def after_sign_out_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
-    :omniauth_callbacks => "users/omniauth_callbacks",
     :registrations => "users/registrations",
     :sessions => "users/sessions",
     :confirmations => "users/confirmations",
