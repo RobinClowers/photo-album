9e55064337f3825b9be31fae214930c5bafc7c0d
Robin Clowers <robin.clowers@gmail.com>
2021-04-25 16:26:54 -0700

    Remove omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-    :validatable, :confirmable, :omniauthable, omniauth_providers: %i[facebook]
+    :validatable, :confirmable
 
   attribute :profile_photo_url
 
   validates :provider, :name, presence: true
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.name = auth.info.name
-      user.password = Devise.friendly_token[0,20]
-      user.skip_confirmation!
-    end
-  end
 
   def signed_in?
     true
