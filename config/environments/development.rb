2240b0ac2ffa9d4c85787120f1be890b04fc49d3
Robin Clowers <robin.clowers@gmail.com>
2021-08-31 21:40:23 -0700

    Redundant cors settings removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,12 +58,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # CORS settings
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "localhost:3000"
-      resource "*", headers: :any, methods: :any, credentials: true
-    end
-  end
 end
